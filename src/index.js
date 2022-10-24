c812e3cf51d2cd8320ab3da8a831a867d6f4493d
Shah Arsalan <shaharslan2278@gmail.com>
2022-10-24 19:50:34 +0530

    added image asset

--- src/index.js
+++ src/index.js
@@ -1,7 +1,9 @@
 import App from "./App";
+import Logo from "./logo.svg";
 
 const root = document.getElementById("root");
 root.innerHTML = `
+<img src="${Logo}" alt="build icon" />
 <h1> Your very own webpack for starting your project</h1>
 ${App({ name: "Tanay" })}
 `;
