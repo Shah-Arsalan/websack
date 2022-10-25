788e6f2b805dc08bf8e4f928813e693617c73670
Shah Arsalan <shaharslan2278@gmail.com>
2022-10-25 10:47:32 +0530

    added styling

--- src/app.js
+++ src/app.js
@@ -1,9 +1,16 @@
 import _ from "lodash";
 import "./app.css";
+import Logo from "./logo.svg";
 
 const App = ({ name}) => {
 	return `
-    <h2> Welcome, ${name} macha </h2>
+    <div id="content">
+    <img src="${Logo}" alt="build icon" />
+    <h2> Welcome ${name} to WebSack👋 </h2>
+<h1> Your very own webpack for building your dream project</h1>
+<a href="https://github.com/Shah-Arsalan/websack">Github 🔗</a>
+    
+    <div>
     `;
 };
 export default App;
--- src/index.js
+++ src/index.js
@@ -3,7 +3,5 @@ import Logo from "./logo.svg";
 
 const root = document.getElementById("root");
 root.innerHTML = `
-<img src="${Logo}" alt="build icon" />
-<h1> Your very own webpack for starting your project</h1>
 ${App({ name: "Web Dev" })}
 `;
